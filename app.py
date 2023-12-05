079dc7979da58a22bedd1116dddf3920df4ee356
jleon253 <jleon253@gmail.com>
2023-12-05 06:12:26 -0500

    Agencies list was implemented as GET endpoint

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 from joblib import load
 from numpy import int64
 
+from constants import Agencies
+
 
 # load model
-model_LogisticRegression = load('model/LogisticRegression/model_LogisticRegression.joblib')
-model_OneClassSVM = load('model/OneClassSVM/model_OneClassSVM.joblib')
+model_LogisticRegression = load(
+    "model/LogisticRegression/model_LogisticRegression.joblib"
+)
+model_OneClassSVM = load("model/OneClassSVM/model_OneClassSVM.joblib")
+
 
 def get_prediction(model, params, isCVM=False):
-  x = [params]
-  pred = model.predict(x)
-  y = pred[0]
-  prob = None if isCVM else model.predict_proba(x)[0].tolist()
+    x = [params]
+    pred = model.predict(x)
+    y = pred[0]
+    prob = None if isCVM else model.predict_proba(x)[0].tolist()
 
-  return {
-    'prediction': int(y),
-    'probability': prob
-  }
+    return {"prediction": int(y), "probability": prob}
+
 
 # Initiate API
 app = Flask(__name__)
@@ -25,46 +28,53 @@ app = Flask(__name__)
 
 # Define Endpoints
 
-@app.route('/')
+
+@app.route("/")
 def home():
-  return { 'Helo': 'World' }
+    return {"Helo": "World"}
 
 
-@app.route('/predict-logistic', methods=['POST'])
+@app.route("/predict-logistic", methods=["POST"])
 def predictLogistic():
   params = request.get_json()
-  print('Params', params)
+  print("Params", params)
   values = [
-    params['maneraMorir'],
-    params['tipoAmenaza'],
-    params['estadoHuida'],
-    params['armadoCon'],
-    params['ubicacionExacta'],
-    params['edad'],
-    params['genero'],
-    params['raza'],
-    params['enfermedadMental'],
-    params['camaraCorporal'],
-    params['idAgencias'],
-  ];
+      params["maneraMorir"],
+      params["tipoAmenaza"],
+      params["estadoHuida"],
+      params["armadoCon"],
+      params["ubicacionExacta"],
+      params["edad"],
+      params["genero"],
+      params["raza"],
+      params["enfermedadMental"],
+      params["camaraCorporal"],
+      params["idAgencias"],
+  ]
   pred = get_prediction(model_LogisticRegression, values)
   return pred
 
-@app.route('/predict-svm', methods=['POST'])
+
+@app.route("/predict-svm", methods=["POST"])
 def predictSVM():
   params = request.get_json()
   values = [
-    params['maneraMorir'],
-    params['tipoAmenaza'],
-    params['estadoHuida'],
-    params['armadoCon'],
-    params['ubicacionExacta'],
-    params['edad'],
-    params['genero'],
-    params['raza'],
-    params['enfermedadMental'],
-    params['camaraCorporal'],
-    params['idAgencias'],
-  ];
+      params["maneraMorir"],
+      params["tipoAmenaza"],
+      params["estadoHuida"],
+      params["armadoCon"],
+      params["ubicacionExacta"],
+      params["edad"],
+      params["genero"],
+      params["raza"],
+      params["enfermedadMental"],
+      params["camaraCorporal"],
+      params["idAgencias"],
+  ]
   pred = get_prediction(model_OneClassSVM, values, True)
   return pred
+
+
+@app.route("/agencies", methods=["GET"])
+def getAgencies():
+  return jsonify(Agencies.LIST)
